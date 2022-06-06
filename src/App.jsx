42d7896543ab35e5b2d97f7afbf66ef8bbdbef4c
YASHV1752 <yashv1752@gmail.com>
2022-06-06 09:51:33 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+          <h1>Mobile Operating System</h1>
+            <ul>
+                <li>Android</li>
+                <li>Blackberry</li>
+                <li>ios</li>
+                <li>Windows</li>
+            </ul>
+            <h1>Mobile Manufacturers</h1>
+            <ul>
+                <li>Samsung</li>
+                <li>htc</li>
+                <li>micromax</li>
+                <li>apple</li>
+            </ul>
+    </div>
+  );
+}
+
+export default App;
